20e91a9c20e8378da10bdd0afaa393369640cd3f
ballader <103984796+ballader@users.noreply.github.com>
2023-04-23 23:22:43 +0000

    added dynamic data and api route

--- main.py
+++ main.py
@@ -1,39 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
-COURSES = [
-  {
-    'id': 1,
-    'title': '8th std',
-    'location': 'calicut',
-    'fee': '10000'
-  }
-  {
-    'id': 2,
-    'title': '9th std',
-    'location': 'calicut',
-    'fee': '12000'
-  }
-  {
-    'id': 3,
-    'title': '10th std',
-    'location': 'calicut',
-    'fee': '13000'
-  }
-  {
-    'id': 4,
-    'title': '11th std',
-    'location': 'calicut',
-    'fee': '14000'
-  }
-]
+COURSES = [  {    'id': 1,    'title': '8th std',    'location': 'calicut',    'fee': '10000'  },  {    'id': 2,    'title': '9th std',    'location': 'calicut',    'fee': '12000'  },  {    'id': 3,    'title': '10th std',    'location': 'calicut',    'fee': '13000'  },  {    'id': 4,    'title': '11th std',    'location': 'calicut',    'fee': '14000'  }]
+
 
 @app.route("/") # "/"here stands for routing to home page of the url
 def hello_world():
-  return render_template('home.html', jobs=JOBS)
+  return render_template('home.html', jobs=COURSES)
 
-print(__name__)
+@app.route("/api/courses")
+def list_jobs():
+  return jsonify(COURSES)
+
+
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
