c7bc5c532e1f04efb4ba38da03abdd740c2965c0
ballader <103984796+ballader@users.noreply.github.com>
2023-04-23 20:56:32 +0000

    added bootsrap

--- main.py
+++ main.py
@@ -2,9 +2,36 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+COURSES = [
+  {
+    'id': 1,
+    'title': '8th std',
+    'location': 'calicut',
+    'fee': '10000'
+  }
+  {
+    'id': 2,
+    'title': '9th std',
+    'location': 'calicut',
+    'fee': '12000'
+  }
+  {
+    'id': 3,
+    'title': '10th std',
+    'location': 'calicut',
+    'fee': '13000'
+  }
+  {
+    'id': 4,
+    'title': '11th std',
+    'location': 'calicut',
+    'fee': '14000'
+  }
+]
+
 @app.route("/") # "/"here stands for routing to home page of the url
 def hello_world():
-  return render_template('home.html')
+  return render_template('home.html', jobs=JOBS)
 
 print(__name__)
 
