97c732aff179deef47c15656a2c840577f5afb76
ballader <103984796+ballader@users.noreply.github.com>
2023-04-23 18:49:41 +0000

    added html and css

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/") # "/"here stands for routing to home page of the url
 def hello_world():
-  return "hello, welcome to Graviton.Edu"
+  return render_template('home.html')
+
+print(__name__)
 
 if __name__ == "__main__":
-  app.run(host='0.0.0.0', debug=True) #this is work arund to run the application instead of invoking the flask command and setting the environmental variable to run the web application
+  app.run(host='0.0.0.0', debug=True)
+  print("im inside if")#this is work arund to run the application instead of invoking the flask command and setting the environmental variable to run the web application
   
   
